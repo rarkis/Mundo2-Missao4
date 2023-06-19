ef7ef0975ebd3542e56bd747d569198cc2ad1860
Alison <202208906893@alunos.estacio.br>
2023-06-18 22:43:24 -0300

    LivroListaComponent

--- src/app/livro-lista/livro-lista.component.ts
+++ src/app/livro-lista/livro-lista.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Editora } from '../editora';
+import { Livro } from '../livro';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
 
 @Component({
   selector: 'app-livro-lista',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./livro-lista.component.css']
 })
 export class LivroListaComponent {
+  public editoras: Editora[] = [];
+  public livros: Livro[] = [];
+  
+  constructor( private servEditora: ControleEditoraService, private servLivros: ControleLivrosService){
+  };
 
+  ngOnInit():void {
+    this.editoras = this.servEditora.getEditoras();
+    this.livros = this.servLivros.obterLivros();
+  };
+
+  excluir = (codigo:number): void  =>{
+    this.servLivros.excluir(codigo);
+    this.livros = this.servLivros.obterLivros();
+  };
+
+  obterNome = (codEditora:number): string =>{
+    return this.servEditora.getNomeEditora(codEditora);
+  }
 }
